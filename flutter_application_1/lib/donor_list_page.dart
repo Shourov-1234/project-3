2007df105f5208f452a295ac6a147e12cae51706
Shourov kazi <143107484+Shourov-1234@users.noreply.github.com>
2025-02-23 12:07:44 +0600

    dfsf

--- flutter_application_1/lib/donor_list_page.dart
+++ flutter_application_1/lib/donor_list_page.dart
@@ -3,52 +3,29 @@ import 'donor_details_page.dart';
 
 class DonorListPage extends StatelessWidget {
   final List<Map<String, String>> donors = [
-    {'name': 'Rahat', 'bloodGroup': 'O+'},
-    {'name': 'Ridoy', 'bloodGroup': 'A+'},
-    {'name': 'Kazi', 'bloodGroup': 'B-'},
+    {'name': 'Tabib', 'bloodGroup': 'O+'},
+    {'name': 'Rahim', 'bloodGroup': 'A+'},
+    {'name': 'Karim', 'bloodGroup': 'B-'}
   ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Donor List")),
-      body: GridView.builder(
-        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2,
-          crossAxisSpacing: 4.0,
-          mainAxisSpacing: 4.0,
-        ),
+      body: ListView.builder(
         itemCount: donors.length,
         itemBuilder: (context, index) {
-          return Card(
-            color: Colors.blue,
-            elevation: 2.0,
-            margin: EdgeInsets.all(4.0),
-            child: Padding(
-              padding: EdgeInsets.all(8.0),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Text(
-                    donors[index]['name']!,
-                    style: TextStyle(
-                      fontSize: 14,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white,
-                    ),
-                  ),
-                  SizedBox(height: 4),
-                  Text(
-                    "Blood Group: ${donors[index]['bloodGroup']}",
-                    style: TextStyle(
-                      fontSize: 12,
-                      color: Colors.white,
-                    ),
-                  ),
-                ],
-              ),
-            ),
+          return ListTile(
+            title: Text(donors[index]['name']!),
+            subtitle: Text("Blood Group: ${donors[index]['bloodGroup']}"),
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => DonorDetailsPage(donor: donors[index]),
+                ),
+              );
+            },
           );
         },
       ),
