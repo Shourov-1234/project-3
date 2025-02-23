1d3cb32e649d29585800f2e4a2b3e348c1a5ea79
Ridoyhassan77 <imridoyhassan@gmail.com>
2025-02-23 11:46:12 +0600

    ss

--- flutter_application_1/lib/donor_details_page.dart
+++ flutter_application_1/lib/donor_details_page.dart
@@ -1,1 +1,27 @@
-// TODO Implement this library.
+import 'package:flutter/material.dart';
+
+class DonorDetailsPage extends StatelessWidget {
+  final Map<String, String> donor;
+
+  DonorDetailsPage({required this.donor});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Donor Details")),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text("Name: ${donor['name']}", style: TextStyle(fontSize: 18)),
+            Text("Blood Group: ${donor['bloodGroup']}",
+                style: TextStyle(fontSize: 18)),
+            Text("Mobile: ---", style: TextStyle(fontSize: 18)),
+            Text("Address: ---", style: TextStyle(fontSize: 18)),
+          ],
+        ),
+      ),
+    );
+  }
+}
